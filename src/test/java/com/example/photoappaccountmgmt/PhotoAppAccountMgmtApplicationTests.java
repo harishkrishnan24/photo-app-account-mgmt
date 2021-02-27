08e2ad1b8c02245fbf89b72ddf4e2985e2006974
Harish krishnan <harishkrishnan1993@gmail.com>
2021-02-27 17:21:17 +0530

    Enable discovery client

---
+++ src/test/java/com/example/photoappaccountmgmt/PhotoAppAccountMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photoappaccountmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppAccountMgmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
